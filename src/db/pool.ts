7d97eddea64dd6fc366987f308f2bec93c01fde9
Adrianus Vieira <adrianuscardoso@gmail.com>
2026-07-06 09:22:50 -0300

    feat: tryinf to fix CORS error

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -5,3 +5,7 @@ export const pool = new Pool({
   connectionString: env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
+
+pool.on("error", (error) => {
+  console.error("Unexpected error on idle Postgres client", error);
+});
